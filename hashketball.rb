57d56053d93568b116609b5bf8a2c1c4e0456c7c
Teejay Dixon <tahjgdixon@gmail.com>
2022-09-19 15:27:17 -0700

    commit tree ec47767f61a7a72a5008d1845af558b971239f8a

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +128,13 @@ def game_hash
   }
 end
 
-# Write code here
+
+
+def 
+team_data.each do |player,  players|
+
+
+end
+binding.pry
+
+
